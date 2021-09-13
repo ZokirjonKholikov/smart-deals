517d6e635660037677d85aabc39820e76254e202
xoliqov-zokirjon <zokirjonxoliqov97@gmail.com>
2021-09-13 22:33:10 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+  // tailwind.config.js
+  module.exports = {
+    purge: [
+      './src/**/*.html',
+      './src/**/*.js',
+    ],
+     darkMode: false, // or 'media' or 'class'
+     theme: {
+       extend: {},
+     },
+     screen:{
+       
+     },
+     variants: {},
+     plugins: [],
+   }
